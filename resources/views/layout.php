976a0a3863bdffe1c30a672f751979734a3045fd
Estênio Mariano <emso.exe@gmail.com>
2019-06-14 13:07:45 -0300

    Adição do JQuery e edição de logo

--- resources/views/layout.php
+++ resources/views/layout.php
@@ -33,9 +33,10 @@
 
         </div>
 
-		<!-- Bootstrap JS form CDN -->
-		<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-		<script>window.jQuery || document.write('<script src="js/vendor/jquery-3.2.1.min.js"><\/script>')</script>
+		<!-- JQuery -->
+		<script src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
+		<script>window.jQuery || document.write('<script src="js/jquery-3.4.1.min.js"><\/script>')</script>
+
 		<!-- Bootstrap Popper form CDN -->
 		<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
 
