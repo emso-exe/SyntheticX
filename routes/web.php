9e7983d0e21495a3fb5738102874ea18f0958e94
Estênio Mariano <emso.exe@gmail.com>
2019-08-04 04:18:28 -0300

    Edição rotas web.php.

--- routes/web.php
+++ routes/web.php
@@ -7,7 +7,7 @@ use app\classes\Routes;
 use app\classes\Uri;
 
 $routes = [
-
+    
     '/'                            => 'app/controllers/login',
     '/dashboard'                   => 'app/controllers/dashboard',
     '/user_create'                 => 'app/controllers/user_create',
