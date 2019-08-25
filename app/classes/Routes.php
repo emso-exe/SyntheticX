54a89db6882545681eb1169d39d8ae9c9d7164d4
Estênio Mariano <emso.exe@gmail.com>
2019-08-25 20:34:26 -0300

    Ajustes de indentação.

--- app/classes/Routes.php
+++ app/classes/Routes.php
@@ -4,17 +4,13 @@ namespace app\classes;
 
 class Routes
 {
-
     public static function load($routes, $uri)
     {
-
         if (!array_key_exists($uri, $routes)) {
 
             throw new \Exception("Rota não existe {$uri}");
-
         }
 
         return "../{$routes[$uri]}.php";
-
     }
 }
