71b0b69caf0d2d9366195414e835fc0c001de186
Estênio Mariano <emso.exe@gmail.com>
2019-08-28 01:26:47 -0300

    Ajuste nas querys select.

--- app/controllers/user_search.php
+++ app/controllers/user_search.php
@@ -1,14 +1,19 @@
 <?php
 
-use app\models\Table;
+use app\models\Query;
 use app\classes\Layout;
 
-$user = new Table;
+$user = new Query;
+$user->createQuery(
+    "select * from usuario as u
+    inner join login as l on u.id_matricula = l.id_matricula
+    inner join cargo as c on u.id_cargo = c.id_cargo
+    inner join setor as s on c.id_setor = s.id_setor
+    inner join perfil as p on c.id_perfil = p.id_perfil
+    inner join status as st on u.id_status = st.id_status;"
+);
 
-//dd($user->all());
-
-$user = $user->all();
-var_dump($user);
+$users = $user->all();
 
 $layout->add('layout_content');
 
