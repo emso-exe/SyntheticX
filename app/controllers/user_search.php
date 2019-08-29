3ddba31e68878bfeb16d07bd05b8623e805225d1
Estênio Mariano <emso.exe@gmail.com>
2019-08-28 23:42:20 -0300

    Ajuste da query.

--- app/controllers/user_search.php
+++ app/controllers/user_search.php
@@ -5,12 +5,13 @@ use app\classes\Layout;
 
 $user = new Query;
 $user->createQuery(
-    "select * from usuario as u
-    inner join login as l on u.id_matricula = l.id_matricula
-    inner join cargo as c on u.id_cargo = c.id_cargo
-    inner join setor as s on c.id_setor = s.id_setor
-    inner join perfil as p on c.id_perfil = p.id_perfil
-    inner join status as st on u.id_status = st.id_status;"
+    "SELECT u.id_matricula, nm_usuario, nm_setor, nm_cargo, ds_login, ds_perfil, u.dt_create, u.dt_update, ds_status 
+    FROM usuario AS u
+    INNER JOIN login AS l ON u.id_matricula = l.id_matricula
+    INNER JOIN cargo AS c ON u.id_cargo = c.id_cargo
+    INNER JOIN setor AS s ON c.id_setor = s.id_setor
+    INNER JOIN perfil AS p ON c.id_perfil = p.id_perfil
+    INNER JOIN status AS st ON u.id_status = st.id_status;"
 );
 
 $users = $user->all();
