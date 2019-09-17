cc3a7a0844b06aebd2ccb5f136cef712248e22a0
Estênio Mariano <emso.exe@gmail.com>
2019-09-17 02:50:10 -0300

    Adicionado chamada ao javascript table-sorter.

--- resources/views/layout.php
+++ resources/views/layout.php
@@ -49,7 +49,8 @@
 		<!--<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>-->
 
 		<!-- My JS -->
+		<script src="js/jquery.tablesorter.js"></script>
 		<script src="js/all.js"></script>
-		
+
 	</body>
 </html>
