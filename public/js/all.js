bb9701e423a8647b12e874aff655c2566ca8e919
Estênio Mariano <emso.exe@gmail.com>
2019-09-17 02:46:35 -0300

    Adicionado chamada a classe table-sorter.

--- public/js/all.js
+++ public/js/all.js
@@ -1,19 +1,22 @@
-$(function() {
-    $(".my-status").each(function() {
-      var $el = $(this);
-      var op = $el.text();
-      switch (op) {
-        case 'Ativo':
-          $el.html('<i class="fas fa-circle fa-small-size text-success"></i>');
-          break;
-        case 'Inativo':
-          $el.html('<i class="fas fa-circle fa-small-size text-warning"></i>');
-          break;
-        case 'Bloqueado':
-          $el.html('<i class="fas fa-circle fa-small-size text-danger"></i>');
-          break;
-        default:
-          break;
-      }
-    });
+$(function () {
+  $(".my-status").each(function () {
+    var $el = $(this);
+    var op = $el.text();
+    switch (op) {
+      case 'Ativo':
+        $el.html('<i class="fas fa-circle fa-small-size text-success"></i>');
+        break;
+      case 'Inativo':
+        $el.html('<i class="fas fa-circle fa-small-size text-warning"></i>');
+        break;
+      case 'Bloqueado':
+        $el.html('<i class="fas fa-circle fa-small-size text-danger"></i>');
+        break;
+      default:
+        break;
+    }
   });
+
+  $('#my-table').tablesorter();
+
+});
