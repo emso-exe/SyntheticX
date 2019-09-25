69138cbdb390fca2da62ec648e0541dc521312de
Estênio Mariano <emso.exe@gmail.com>
2019-09-25 14:38:03 -0300

    Remoção de scripts não utilizado.

--- public/js/all.js
+++ public/js/all.js
@@ -1,4 +1,5 @@
 $(function () {
+
   $(".my-status").each(function () {
     var $el = $(this);
     var op = $el.text();
@@ -16,9 +17,10 @@ $(function () {
         break;
     }
   });
+
 });
 
-$(function() {
+$(function () {
 
   // **********************************
   //  Description of ALL pager options
@@ -32,7 +34,7 @@ $(function() {
     ajaxUrl: null,
 
     // modify the url after all processing has been applied
-    customAjaxUrl: function(table, url) { return url; },
+    customAjaxUrl: function (table, url) { return url; },
 
     // ajax error callback from $.tablesorter.showError function
     // ajaxError: function( config, xhr, settings, exception ) { return exception; };
@@ -70,12 +72,12 @@ $(function() {
     size: 7,
 
     // Save pager page & size if the storage script is loaded (requires $.tablesorter.storage in jquery.tablesorter.widgets.js)
-    savePages : true,
+    savePages: true,
 
     // Saves tablesorter paging to custom key if defined.
     // Key parameter name used by the $.tablesorter.storage function.
     // Useful if you have multiple tables defined
-    storageKey:'tablesorter-pager',
+    storageKey: 'tablesorter-pager',
 
     // Reset pager to this page after filtering; set to desired page number (zero-based index),
     // or false to not change page at filter start
@@ -113,14 +115,14 @@ $(function() {
     // Initialize tablesorter
     // ***********************
     .tablesorter({
-      theme: 'blue',
-      widthFixed: true,
-      widgets: ['zebra', 'filter']
+      //theme: 'blue',
+      //widthFixed: true,
+      //widgets: ['zebra', 'filter']
     })
 
     // bind to pager events
     // *********************
-    .bind('pagerChange pagerComplete pagerInitialized pageMoved', function(e, c) {
+    .bind('pagerChange pagerComplete pagerInitialized pageMoved', function (e, c) {
       var msg = '"</span> event triggered, ' + (e.type === 'pagerChange' ? 'going to' : 'now on') +
         ' page <span class="typ">' + (c.page + 1) + '/' + c.totalPages + '</span>';
       $('#display')
@@ -132,88 +134,88 @@ $(function() {
     // ****************************
     .tablesorterPager(pagerOptions);
 
-    // Add two new rows using the "addRows" method
-    // the "update" method doesn't work here because not all rows are
-    // present in the table when the pager is applied ("removeRows" is false)
-    // ***********************************************************************
-    var r, $row, num = 50,
-      row = '<tr><td>Student{i}</td><td>{m}</td><td>{g}</td><td>{r}</td><td>{r}</td><td>{r}</td><td>{r}</td><td><button type="button" class="remove" title="Remove this row">X</button></td></tr>' +
-        '<tr><td>Student{j}</td><td>{m}</td><td>{g}</td><td>{r}</td><td>{r}</td><td>{r}</td><td>{r}</td><td><button type="button" class="remove" title="Remove this row">X</button></td></tr>';
-    $('button:contains(Add)').click(function() {
-      // add two rows of random data!
-      r = row.replace(/\{[gijmr]\}/g, function(m) {
-        return {
-          '{i}' : num + 1,
-          '{j}' : num + 2,
-          '{r}' : Math.round(Math.random() * 100),
-          '{g}' : Math.random() > 0.5 ? 'male' : 'female',
-          '{m}' : Math.random() > 0.5 ? 'Mathematics' : 'Languages'
-        }[m];
-      });
-      num = num + 2;
-      $row = $(r);
-      $('table')
-        .find('tbody').append($row)
-        .trigger('addRows', [$row]);
-      return false;
+  // Add two new rows using the "addRows" method
+  // the "update" method doesn't work here because not all rows are
+  // present in the table when the pager is applied ("removeRows" is false)
+  // ***********************************************************************
+  var r, $row, num = 50,
+    row = '<tr><td>Student{i}</td><td>{m}</td><td>{g}</td><td>{r}</td><td>{r}</td><td>{r}</td><td>{r}</td><td><button type="button" class="remove" title="Remove this row">X</button></td></tr>' +
+      '<tr><td>Student{j}</td><td>{m}</td><td>{g}</td><td>{r}</td><td>{r}</td><td>{r}</td><td>{r}</td><td><button type="button" class="remove" title="Remove this row">X</button></td></tr>';
+  $('button:contains(Add)').click(function () {
+    // add two rows of random data!
+    r = row.replace(/\{[gijmr]\}/g, function (m) {
+      return {
+        '{i}': num + 1,
+        '{j}': num + 2,
+        '{r}': Math.round(Math.random() * 100),
+        '{g}': Math.random() > 0.5 ? 'male' : 'female',
+        '{m}': Math.random() > 0.5 ? 'Mathematics' : 'Languages'
+      }[m];
     });
+    num = num + 2;
+    $row = $(r);
+    $('table')
+      .find('tbody').append($row)
+      .trigger('addRows', [$row]);
+    return false;
+  });
 
-    // Delete a row
-    // *************
-    $('table').delegate('button.remove', 'click' ,function() {
-      var t = $('table');
-      // disabling the pager will restore all table rows
-      // t.trigger('disablePager');
-      // remove chosen row
-      $(this).closest('tr').remove();
-      // restore pager
-      // t.trigger('enablePager');
-      t.trigger('update');
-      return false;
-    });
+  // Delete a row
+  // *************
+  $('table').delegate('button.remove', 'click', function () {
+    var t = $('table');
+    // disabling the pager will restore all table rows
+    // t.trigger('disablePager');
+    // remove chosen row
+    $(this).closest('tr').remove();
+    // restore pager
+    // t.trigger('enablePager');
+    t.trigger('update');
+    return false;
+  });
 
-    // Destroy pager / Restore pager
-    // **************
-    $('button:contains(Destroy)').click(function() {
-      // Exterminate, annhilate, destroy! http://www.youtube.com/watch?v=LOqn8FxuyFs
-      var $t = $(this);
-      if (/Destroy/.test( $t.text() )) {
-        $('table').trigger('destroyPager');
-        $t.text('Restore Pager');
-      } else {
-        $('table').tablesorterPager(pagerOptions);
-        $t.text('Destroy Pager');
-      }
-      return false;
-    });
+  // Destroy pager / Restore pager
+  // **************
+  $('button:contains(Destroy)').click(function () {
+    // Exterminate, annhilate, destroy! http://www.youtube.com/watch?v=LOqn8FxuyFs
+    var $t = $(this);
+    if (/Destroy/.test($t.text())) {
+      $('table').trigger('destroyPager');
+      $t.text('Restore Pager');
+    } else {
+      $('table').tablesorterPager(pagerOptions);
+      $t.text('Destroy Pager');
+    }
+    return false;
+  });
 
-    // Disable / Enable
-    // **************
-    $('.toggle').click(function() {
-      var mode = /Disable/.test( $(this).text() );
-      $('table').trigger( (mode ? 'disable' : 'enable') + 'Pager');
-      $(this).text( (mode ? 'Enable' : 'Disable') + 'Pager');
-      return false;
-    });
-    $('table').bind('pagerChange', function() {
-      // pager automatically enables when table is sorted.
-      $('.toggle').text('Disable Pager');
-    });
+  // Disable / Enable
+  // **************
+  $('.toggle').click(function () {
+    var mode = /Disable/.test($(this).text());
+    $('table').trigger((mode ? 'disable' : 'enable') + 'Pager');
+    $(this).text((mode ? 'Enable' : 'Disable') + 'Pager');
+    return false;
+  });
+  $('table').bind('pagerChange', function () {
+    // pager automatically enables when table is sorted.
+    $('.toggle').text('Disable Pager');
+  });
 
-    // clear storage (page & size)
-    $('.clear-pager-data').click(function() {
-      // clears user set page & size from local storage, so on page
-      // reload the page & size resets to the original settings
-      $.tablesorter.storage( $('table'), 'tablesorter-pager', '' );
-    });
+  // clear storage (page & size)
+  $('.clear-pager-data').click(function () {
+    // clears user set page & size from local storage, so on page
+    // reload the page & size resets to the original settings
+    $.tablesorter.storage($('table'), 'tablesorter-pager', '');
+  });
 
-    // go to page 1 showing 10 rows
-    $('.goto').click(function() {
-      // triggering "pageAndSize" without parameters will reset the
-      // pager to page 1 and the original set size (10 by default)
-      // $('table').trigger('pageAndSize')
-      //$('table').trigger('pageAndSize', [1, 10]);
-      $('table').trigger('pageAndSize', [1, 7]);
-    });
+  // go to page 1 showing 10 rows
+  $('.goto').click(function () {
+    // triggering "pageAndSize" without parameters will reset the
+    // pager to page 1 and the original set size (10 by default)
+    // $('table').trigger('pageAndSize')
+    //$('table').trigger('pageAndSize', [1, 10]);
+    $('table').trigger('pageAndSize', [1, 7]);
+  });
 
 });
