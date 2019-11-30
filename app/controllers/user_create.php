9d8a44e31213c13a76cf40b56b705f603d21f40e
Estênio Mariano <emso.exe@gmail.com>
2019-11-30 19:20:48 -0300

    Chamada do método insert.

--- app/controllers/user_create.php
+++ app/controllers/user_create.php
@@ -5,20 +5,16 @@ use app\classes\Validation;
 use app\models\Query;
 
 $validation = new Validation;
-$validate = $validation->validate($_POST);
+$validate   = $validation->validate($_POST);
 
 $user = new Query;
 
+$cadastrado = $user->createInsert('usuario', $validate);
 
-/*require "../app/functions/helpers.php";
-dd($user->all());*/
-
-//$users = $user->all();
+//$new_user = $user->insert();
 
 $layout->add('layout_content');
 
 $content = new Layout;
 
 $content->add('user_create');
-
-//require "../app/views/index.php";
