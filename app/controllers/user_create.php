34b89fe6597dd4c8079cdc1bb60ab769fd1dcd31
Estênio Mariano <emso.exe@gmail.com>
2019-12-04 18:32:06 -0300

    Correção da passagem e validação de atributos.

--- app/controllers/user_create.php
+++ app/controllers/user_create.php
@@ -4,14 +4,31 @@ use app\classes\Layout;
 use app\classes\Validation;
 use app\models\Query;
 
-$validation = new Validation;
-$validate   = $validation->validate($_POST);
+if (!empty($_POST)) {
 
-$user = new Query;
+    $validation  = new Validation;
+    $validate    = $validation->validate($_POST);
+    
+    $attributes1 = [
+        'id_matricula' => $validate->{'id_matricula'},
+        'nm_usuario'   => $validate->{'nm_usuario'},
+        'id_cargo'     => $validate->{'id_cargo'},
+        'id_status'    => $validate->{'id_status'} = 1,
+    ];
 
-$user->createInsert('usuario', $validate);
+    $attributes2 = [
+        'id_matricula' => $validate->{'id_matricula'},
+        'ds_login'     => $validate->{'ds_login'},
+        'ps_senha'     => $validate->{'ps_senha'} = 0,
+    ];
 
-//$new_user = $user->insert();
+    $user = new Query;
+
+    $user->createInsert('usuario', $attributes1);
+    $user->insert();
+    $user->createInsert('login', $attributes2);
+    $user->insert();
+}
 
 $layout->add('layout_content');
 
