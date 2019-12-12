8a2df593694be69fbf5d697ee5b080b31d53063d
Estênio Mariano <emso.exe@gmail.com>
2019-12-11 23:33:16 -0300

    Preenchimento dos selects do form via php.

--- resources/views/user_create.php
+++ resources/views/user_create.php
@@ -19,25 +19,32 @@
 
         	<div class="form-group">
 				<label for="nm_login" class="col-md-2 col-form-label col-form-label-lg">Login</label>
-				<input class="col-md-11 ml-3 form-control form-control-lg" id="nm_login" type="text" name="ds_login" placeholder="ds_login" onkeypress="" required>
+				<div class="input-group">
+					<input class="col-md-9 ml-3 form-control form-control-lg" id="nm_login" aria-describedby="nm_login" type="text" name="ds_login" placeholder="ds_login" onkeypress="" required>
+  					<div class="input-group-append">
+    					<button class="px-4 btn btn-secondary" type="button" id="nm_login">&nbsp;&nbsp;&nbsp;&nbsp;GERAR LOGIN&nbsp;&nbsp;&nbsp;&nbsp;</button>
+  					</div>
+				</div>
 			</div>
 
 			<div class="form-row">
     			<div class="form-group col-md-6">
 					<label for="cd_setor" class="col-md-2 col-form-label col-form-label-lg">Setor</label>
-      				<select class="col-md-10 ml-3 form-control form-control-lg" id="setor" type="text" name="id_setor" placeholder="id_setor" onchange="" required>
-						<option value="">Selecione um setor</option>
-						<option value="1">CPD</option>
-						<option value="">Suporte</option>
+      				<select class="col-md-10 ml-3 form-control form-control-lg" id="id_setor" type="text" name="id_setor" placeholder="id_setor" onchange="" required>
+						<option value="0">Selecione um setor</option>
+						<?php foreach ($sector as $option): ?>
+							<option value="<?=$option->id_setor?>"><?=$option->nm_setor?></option>
+						<?php endforeach?>
 					</select>
 				</div>
 
     			<div class="form-group col-md-6" >
 					<label for="cd_cargo" class="col-md-2 col-form-label col-form-label-lg">Cargo</label>
-      				<select class="col-md-10 ml-3 form-control form-control-lg" id="cd_cargo" type="text" name="id_cargo" placeholder="id_cargo" onchange="" required>
+      				<select class="col-md-10 ml-3 form-control form-control-lg" id="id_cargo" type="text" name="id_cargo" placeholder="id_cargo" onchange="" required>
 						<option value="0">Selecione um cargo</option>
-						<option value="1">Operador de CPD</option>
-						<option value="0">Analista de suporte</option>
+						<?php foreach ($position as $option): ?>
+							<option data-position="<?=$option->id_setor?>" value="<?=$option->id_cargo?>"><?=$option->nm_cargo?></option>
+						<?php endforeach?>
 					</select>
 				</div>
 			</div>
