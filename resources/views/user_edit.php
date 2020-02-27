ef6ca0f74ff36ebe32456e76ab112ea116f97ce9
Estênio Mariano <emso.exe@gmail.com>
2020-02-27 18:23:13 -0300

    Exibição dos dados que devem ser alterados nos selects.

--- resources/views/user_edit.php
+++ resources/views/user_edit.php
@@ -10,52 +10,62 @@
 			<div class="form-row">
 				<div class="form-group col-md-6">
 					<label for="cd_matricula" class="col-md-2 col-form-label col-form-label-lg">Matrícula</label>
-					<input class="col-md-10 ml-3 form-control form-control-lg" id="cd_matricula" type="text" name="cd_matricula" placeholder="cd_matricula" onkeypress="" required>
+					<input value="<?=$found[0]->id_matricula?>" class="col-md-10 ml-3 form-control form-control-lg" id="id_matricula" type="text" name="cd_matricula" placeholder="cd_matricula" onkeypress="" required>
 				</div>
 				<div class="form-group col-md-6">
 						<label for="cd_status" class="col-md-2 col-form-label col-form-label-lg">Status</label>
-						<select class="col-md-10 ml-3 form-control form-control-lg" id="cd_status" type="text" name="cd_status" placeholder="cd_status" onchange="" required>
-							<option value="0">Ativo</option>
-							<option value="1">Inativo</option>
-							<option value="2">Bloqueado</option>
+						<select class="col-md-10 ml-3 form-control form-control-lg" id="id_status" type="text" name="id_status" placeholder="id_status" onchange="" required>
+						<option value="0">Selecione um status</option>
+						<?php foreach ($status as $option): 
+							($option->id_status==$found[0]->id_status) ? $selection="selected" : $selection=null;
+						?>
+							<option value="<?=$option->id_status?>" <?=$selection?>><?=$option->ds_status?></option>
+						<?php endforeach?>
 						</select>
 				</div>
 			</div>
 
         	<div class="form-group">
 				<label for="nm_nome" class="col-md-2 col-form-label col-form-label-lg">Nome</label>
-				<input class="col-md-11 ml-3 form-control form-control-lg" id="nm_nome" type="text" name="nm_nome" placeholder="nm_nome" onkeypress="" required>
+				<input value="<?=$found[0]->nm_usuario?>" class="col-md-11 ml-3 form-control form-control-lg" id="nm_nome" type="text" name="nm_nome" placeholder="nm_nome" onkeypress="" required>
 			</div>
 
         	<div class="form-group">
         		<div class="form-row justify-content-between mr-5">
 					<label for="nm_login" class="col-md-2 col-form-label col-form-label-lg ml-3">Login</label>
 				</div>
-				<input class="col-md-11 ml-3 form-control form-control-lg" id="nm_login" type="text" name="nm_login" placeholder="nm_login" onkeypress="" required>
+				<input value="<?=$found[0]->ds_login?>" class="col-md-11 ml-3 form-control form-control-lg" id="nm_login" type="text" name="nm_login" placeholder="nm_login" onkeypress="" required>
 			</div>
 
-			<div class="form-row">
+			<div class="form-row mt-4">
     			<div class="form-group col-md-6">
 					<label for="cd_setor" class="col-md-2 col-form-label col-form-label-lg">Setor</label>
-      				<select class="col-md-10 ml-3 form-control form-control-lg" id="setor" type="text" name="cd_setor" placeholder="cd_setor" onchange="" required>
-						<option value="">Selecione um setor</option>
-						<option value="">CPD</option>
-						<option value="">Suporte</option>
+      				<select class="col-md-10 ml-3 form-control form-control-lg validation" id="id_setor" type="text" name="id_setor" placeholder="id_setor" onchange="" required>
+						<option value="0">Selecione um setor</option>
+						<?php foreach ($sector as $option): 
+							($option->id_setor==$found[0]->id_setor) ? $selection="selected" : $selection=null;
+						?>
+							<option value="<?=$option->id_setor?>" <?=$selection?>><?=$option->nm_setor?></option>
+						<?php endforeach?>
 					</select>
 				</div>
+
     			<div class="form-group col-md-6" >
 					<label for="cd_cargo" class="col-md-2 col-form-label col-form-label-lg">Cargo</label>
-      				<select class="col-md-10 ml-3 form-control form-control-lg" id="cd_cargo" type="text" name="cd_cargo" placeholder="cd_cargo" onchange="" required>
-						<option value="0">Selecione um cargo</option>
-						<option value="1">Operador de CPD</option>
-						<option value="2">Analista de suporte</option>
+      				<select class="col-md-10 ml-3 form-control form-control-lg validation" id="id_cargo" type="text" name="id_cargo" placeholder="id_cargo" onchange="" required>
+						<option data-position="0" value="0">Selecione um cargo</option>
+						<?php foreach ($position as $option):
+							($option->id_cargo==$found[0]->id_cargo) ? $selection="selected" : $selection=null;
+						?>
+							<option data-position="<?=$option->id_setor?>" value="<?=$option->id_cargo?>" <?=$selection?>><?=$option->nm_cargo?></option>
+						<?php endforeach?>
 					</select>
 				</div>
 			</div>
 
 			<div class="form-row col-md-12 justify-content-center">
 				<button class="btn btn-lg btn-success m-4 col-md-4" type="submit" name="gravar" value="1"><i class="fas fa-save"></i><span class="ml-4">SALVAR</span></button>
-				<button class="btn btn-lg btn-warning m-4 col-md-4" type="reset" name="limpar" value="0"><i class="fas fa-window-close"></i><span class="ml-4">CANCELAR</span></button>
+				<button class="btn btn-lg btn-warning m-4 col-md-4" type="reset" name="cancelar" value="0"><i class="fas fa-window-close"></i><span class="ml-4">CANCELAR</span></button>
 			</div>
 
 		</form>
