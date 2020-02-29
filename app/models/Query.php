8d8ac5257396d36de1dcf977faf809fa479af4fa
Estênio Mariano <emso.exe@gmail.com>
2020-02-29 20:27:06 -0300

    Criação do método para recepção dos dados atualizados.

--- app/models/Query.php
+++ app/models/Query.php
@@ -8,6 +8,7 @@ class Query extends Crud
     protected $sql;
     protected $table;
     protected $attributes;
+    protected $where;
 
     public function createSelect(string $select)
     {
@@ -18,6 +19,13 @@ class Query extends Crud
     {
         $this->table      = $table;
         $this->attributes = (array) $attributes;
+    }
+
+    public function createUpdate($table, $attributes, $where)
+    {
+        $this->table      = $table;
+        $this->attributes = (array) $attributes;
+        $this->where      = $where;
     }
 
 }
