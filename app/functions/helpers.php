d6f12ba79ec6144c21845ec44cefe7f4d2a8db2c
Estênio Mariano <emso.exe@gmail.com>
2020-02-29 20:24:53 -0300

    Criação da função para exbição dados formatados.

--- app/functions/helpers.php
+++ app/functions/helpers.php
@@ -1,10 +1,19 @@
 <?php
 
-function dd($dump)
+function vdd($dump)
 {
 
     var_dump($dump);
 
     die();
 
+}
+
+function prp($dump)
+{
+
+    echo "<pre>";
+    print_r($dump);
+    echo "</pre>";
+    
 }
