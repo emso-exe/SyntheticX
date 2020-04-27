cb627f16cb70436c48f00193561dfe7e3b27e777
Estênio Mariano <emso.exe@gmail.com>
2020-04-27 20:42:49 -0300

    Ajuste de indentação

--- app/classes/Uri.php
+++ app/classes/Uri.php
@@ -11,11 +11,11 @@ class Uri
         return parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
 
     }
-    
+
     public static function make(string $uri)
     {
 
-        return (array)$arr = explode("/", $uri);
+        return (array) $arr = explode("/", $uri);
 
     }
 
