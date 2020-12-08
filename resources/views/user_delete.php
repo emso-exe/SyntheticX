ada8f54819a8005897db964b76d2783bd0f157fc
Estênio Mariano <emso.exe@gmail.com>
2020-12-08 02:24:59 -0300

    Update user_delete.php
    Melhora na visualização do registro a ser excluído.

--- resources/views/user_delete.php
+++ resources/views/user_delete.php
@@ -10,38 +10,38 @@
 		<div class="form-row">
 				<div class="form-group col-md-4">
 					<label for="id_matricula" class="col-md-2 col-form-label col-form-label-lg">Matrícula</label>
-					<p class="col-md-10 ml-3 text-danger" id="id_matricula" name="id_matricula"><?=$found[0]->id_matricula?></p>
+					<p class="col-md-10 ml-3 text-danger alert alert-danger" id="id_matricula" name="id_matricula"><?=$found[0]->id_matricula?></p>
 					<input value="<?=$found[0]->id_matricula?>" id="id_matricula" type="text" name="id_matricula" placeholder="id_matricula" onkeypress="" readonly hidden>
 				</div>
 				<div class="form-group col-md-4">
 					<label for="ds_login" class="col-md-2 col-form-label col-form-label-lg">Login</label>
-					<p class="col-md-11 ml-3 text-danger" id="ds_login" name="ds_login"><?=$found[0]->ds_login?></p>
+					<p class="col-md-11 ml-3 text-danger alert alert-danger" id="ds_login" name="ds_login"><?=$found[0]->ds_login?></p>
 				</div>
 				<div class="form-group col-md-4">
 					<label for="cd_status" class="col-md-2 col-form-label col-form-label-lg">Status</label>
-					<p class="col-md-10 ml-3 text-danger" id="ds_status" name="ds_status"><?=$found[0]->ds_status?></p>
+					<p class="col-md-10 ml-3 text-danger alert alert-danger" id="ds_status" name="ds_status"><?=$found[0]->ds_status?></p>
 				</div>
 			</div>
 
         	<div class="form-group">
 				<label for="nm_usuario" class="col-md-2 col-form-label col-form-label-lg">Nome</label>
-				<p class="col-md-11 ml-3 text-danger" id="nm_usuario" name="nm_usuario"><?=$found[0]->nm_usuario?></p>
+				<p class="col-md-11 ml-3 text-danger alert alert-danger" id="nm_usuario" name="nm_usuario"><?=$found[0]->nm_usuario?></p>
 			</div>
 
 			<div class="form-row">
     			<div class="form-group col-md-6">
 					<label for="cd_setor" class="col-md-2 col-form-label col-form-label-lg">Setor</label>
-      				<p class="col-md-10 ml-3 text-danger" id="nm_setor" name="nm_setor"><?=$found[0]->nm_setor?></p>
+      				<p class="col-md-10 ml-3 text-danger alert alert-danger" id="nm_setor" name="nm_setor"><?=$found[0]->nm_setor?></p>
 				</div>
     			<div class="form-group col-md-6" >
 					<label for="cd_cargo" class="col-md-2 col-form-label col-form-label-lg">Cargo</label>
-      				<p class="col-md-10 ml-3 text-danger" id="nm_cargo" name="nm_cargo"><?=$found[0]->nm_cargo?></p>
+      				<p class="col-md-10 ml-3 text-danger alert alert-danger" id="nm_cargo" name="nm_cargo"><?=$found[0]->nm_cargo?></p>
 				</div>
 			</div>
 
-			<div class="col-md-11 ml-3 mr-3 mt-4 alert alert-danger" role="alert">
+			<div class="col-md-11 ml-3 mr-3 mt-3 alert alert-danger" role="alert">
 				<p class="mt-1 mb-1 text-center">
-					<small><strong>ATENÇÃO!</strong> Ao clicar no botão <strong>EXCLUIR</strong> não será possível restaurar o colaborador&nbsp;<strong><?=$found[0]->nm_usuario?></strong>.</small>
+					<small><strong>ATENÇÃO!</strong> Ao clicar no botão <strong>EXCLUIR</strong> esta ação será irreversível, se existem dúvidas referente a continuidade da operação clique em <strong>CANCELAR</strong>.</small>
 				</p>
   				<hr>
   				<div class="form-row col-md-12 justify-content-center mb-3 mt-4">
@@ -52,6 +52,35 @@
 
 		</form>
 
-	</div>
+        </div>
+
+<div id="modal-message"></div>
+
+<?php
+if ($_POST) {
+echo '<div class="modal fade" id="my-modal" role="dialog">
+    <div class="modal-dialog modal-dialog-centered" role="document">
+        <div class="modal-content">
+            <div class="modal-header bg-danger">
+                <h5 class="modal-title text-white" id=" modalLabel">Usuário excluído</h5>
+            </div>
+            <div class="modal-body">
+                <label>Matrícula:</label><span class="text-danger"> ' . $found[0]->id_matricula . '</span><br>
+                <label>Nome:</label><span class="text-danger"> ' . $found[0]->nm_usuario . '</span><br>
+                <label>Login:</label><span class="text-danger"> ' . $found[0]->ds_login . '</span><br>
+                <label>Setor:</label><span class="text-danger"> ' . $found[0]->nm_setor . '</span><br>
+                <label>Cargo:</label><span class="text-danger"> ' . $found[0]->nm_cargo . '</span><br>
+                <label>Status:</label><span class="text-danger"> ' . $found[0]->ds_status . '</span><br>
+                
+            </div>
+            <div class="modal-footer">
+                <a class="btn btn-danger px-5" href="/user_search/edit">OK</a>
+                <!--<a class="btn btn-primary px-5" href="/user_edit/' . $found[0]->id_matricula . '" data-dismiss="modal">Copiar dados</a>-->
+            </div>
+        </div>
+    </div>
+</div>';
+}
+?>
 
 </div>
